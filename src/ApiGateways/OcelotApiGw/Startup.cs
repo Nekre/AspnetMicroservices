dd53c1f9de8a3ae81ae4a42df57728c523fb1753
Nekre <hrn_tml@outlook.com>
2023-10-01 15:35:58 +0300

    Ocelot APi Gateway

---
+++ src/ApiGateways/OcelotApiGw/Startup.cs
@@ -0,0 +1,46 @@
+﻿
+using Ocelot.DependencyInjection;
+using Ocelot.Middleware;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OcelotApiGw
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+
+            services.AddOcelot();
+           
+
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public async void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+
+            await app.UseOcelot();
+        }
+    }
+}
